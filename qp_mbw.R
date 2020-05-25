1e018e7291fba602b894b84155acb09e4d3a5f42
Sarah Bakst <bakst.sarah@gmail.com>
2020-05-25 08:02:50 -0500

    added r data

--- qp_mbw.R
+++ qp_mbw.R
@@ -42,3 +42,38 @@ names(sbw)[names(sbw)=='framenum']<-'framenum_mbw'
 names(sbw)[names(sbw)=='normdiff']<-'normdiff_mbw'
 names(sbw)[names(sbw)=='normeddiff']<-'normeddiff_mbw'
 
+#############################################################
+
+
+subs<-c('102','103','104','105','108','109','110','112','113','114','115','116','117','118','119','120')
+loadpath = '~/Desktop/vmshare/QP/raw_qp_data/sub_bmps/'
+
+rbw<-NULL
+for (s in subs){
+  rah<-read.csv(paste(loadpath,s,'/rah_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  rome<-read.csv(paste(loadpath,s,'/rome_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  ream<-read.csv(paste(loadpath,s,'/ream_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bar<-read.csv(paste(loadpath,s,'/bar_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bore<-read.csv(paste(loadpath,s,'/bore_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  beer<-read.csv(paste(loadpath,s,'/beer_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bindy<-rbind(rah,rome,ream,bar,bore,beer)
+  rbw<-rbind(rbw,bindy)
+}
+
+subs<-c('121','122','123','124','125','126','127','128')
+loadpath = '~/Desktop/vmshare/QP/raw_qp_data/'
+
+for (s in subs){
+  rah<-read.csv(paste(loadpath,s,'/rah_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  rome<-read.csv(paste(loadpath,s,'/rome_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  ream<-read.csv(paste(loadpath,s,'/ream_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bar<-read.csv(paste(loadpath,s,'/bar_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bore<-read.csv(paste(loadpath,s,'/bore_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  beer<-read.csv(paste(loadpath,s,'/beer_mbw_mask_diffs_byts_frmtime.txt',sep=""))
+  bindy<-rbind(rah,rome,ream,bar,bore,beer)
+  rbw<-rbind(rbw,bindy)
+}
+
+names(rbw)[names(rbw)=='framenum']<-'framenum_mbw'
+names(rbw)[names(rbw)=='normdiff']<-'normdiff_mbw'
+names(rbw)[names(rbw)=='normeddiff']<-'normeddiff_mbw'
