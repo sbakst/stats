d3da8e2a4613ba4fdc5c8ce9010a51531226db35
sbakst <bakst.sarah@gmail.com>
2020-05-25 07:40:00 -0500

    load and bind by-word qp data

---
+++ qp_mbw.R
@@ -0,0 +1,44 @@
+
+subs<-c('103','104','105','108','109','110','112','113','114','115','116','117','118','119','120')
+loadpath = '~/Documents/research/data/QP/raw_qp_data/sib_bmps/'
+
+sbw<-NULL
+for (s in subs){
+   sob<-read.csv(paste(loadpath,s,'/sob_midpt_data.txt',sep=""))
+   sew<-read.csv(paste(loadpath,s,'/sew_midpt_data.txt',sep=""))
+   sea<-read.csv(paste(loadpath,s,'/sea_midpt_data.txt',sep=""))
+   boss<-read.csv(paste(loadpath,s,'/boss_midpt_data.txt',sep=""))
+   dose<-read.csv(paste(loadpath,s,'/dose_midpt_data.txt',sep=""))
+   piece<-read.csv(paste(loadpath,s,'/piece_midpt_data.txt',sep=""))
+   bindy<-rbind(sob,sew,sea,boss,dose,piece)
+   sbw<-rbind(sbw,bindy)
+}
+
+subs<-c('121','122','123','124','125','126','127','128')
+loadpath = '~/Documents/research/data/QP/raw_qp_data/'
+for (s in subs){
+  sob<-read.csv(paste(loadpath,s,'/sob_midpt_data.txt',sep=""))
+  sew<-read.csv(paste(loadpath,s,'/sew_midpt_data.txt',sep=""))
+  sea<-read.csv(paste(loadpath,s,'/sea_midpt_data.txt',sep=""))
+  boss<-read.csv(paste(loadpath,s,'/boss_midpt_data.txt',sep=""))
+  dose<-read.csv(paste(loadpath,s,'/dose_midpt_data.txt',sep=""))
+  piece<-read.csv(paste(loadpath,s,'/piece_midpt_data.txt',sep=""))
+  bindy<-rbind(sob,sew,sea,boss,dose,piece)
+  sbw<-rbind(sbw,bindy)
+}
+
+s<-'102'
+loadpath = '~/Documents/research/data/QP/raw_qp_data/sub_bmps/'
+sob<-read.csv(paste(loadpath,s,'/sob_midpt_data.txt',sep=""))
+sew<-read.csv(paste(loadpath,s,'/sew_midpt_data.txt',sep=""))
+sea<-read.csv(paste(loadpath,s,'/sea_midpt_data.txt',sep=""))
+boss<-read.csv(paste(loadpath,s,'/boss_midpt_data.txt',sep=""))
+dose<-read.csv(paste(loadpath,s,'/dose_midpt_data.txt',sep=""))
+piece<-read.csv(paste(loadpath,s,'/piece_midpt_data.txt',sep=""))
+bindy<-rbind(sob,sew,sea,boss,dose,piece)
+sbw<-rbind(sbw,bindy)
+
+names(sbw)[names(sbw)=='framenum']<-'framenum_mbw'
+names(sbw)[names(sbw)=='normdiff']<-'normdiff_mbw'
+names(sbw)[names(sbw)=='normeddiff']<-'normeddiff_mbw'
+
